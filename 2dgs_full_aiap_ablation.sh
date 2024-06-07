c66bf0451525527774697cd5144072993e212e3c
Sihan Chen <sihanchen2001222@gmail.com>
2024-06-07 23:30:57 +0200

    update 2dgs_ablation script

--- 2dgs_full_aiap_ablation.sh
+++ 2dgs_full_aiap_ablation.sh
@@ -2,27 +2,28 @@
 trap 'echo "Script interrupted"; exit' INT
 
 datasets=("386" "387" "393")
-lambda_aiap_xyz=("0.0" "1.0")
-lambda_aiap_cov=("0.0" "0.001")
+
+declare -A params
+params=( ["pair1"]="0.0 0.0" ["pair2"]="1.0 0.001" )
+
 for dataset in "${datasets[@]}"; do
-  for lambda_xyz in "${lambda_aiap_xyz[@]}"; do
-    for lambda_cov in "${lambda_aiap_cov[@]}"; do
-      train_dataset="zjumocap_${dataset}_mono_half"
-      checkpoint="./exp/${dataset}_2dgs_full_aiap_ablation_${lambda_xyz}_${lambda_cov}"
+  for pair in "${!params[@]}"; do
+    IFS=' ' read -r lambda_xyz lambda_cov <<< "${params[$pair]}"
 
-      python train.py dataset="${train_dataset}" \
-                      opt.lambda_aiap_xyz="${lambda_xyz}" \
-                      opt.lambda_aiap_cov="${lambda_cov}" \
-                      +exp_dir=${checkpoint} \
+    train_dataset="zjumocap_${dataset}_mono_half"
+    checkpoint="./exp/${dataset}_2dgs_full_aiap_ablation_${lambda_xyz}_${lambda_cov}"
 
+    python train.py dataset="${train_dataset}" \
+                    opt.lambda_aiap_xyz="${lambda_xyz}" \
+                    opt.lambda_aiap_cov="${lambda_cov}" \
+                    +exp_dir=${checkpoint} \
 
-      test_dataset="zjumocap_${dataset}_mono_eval_novel"
-      python render.py mode=test \
-                       dataset.test_mode=view \
-                       dataset="${test_dataset}" \
-                       opt.lambda_aiap_xyz="${lambda_xyz}" \
-                       opt.lambda_aiap_cov="${lambda_cov}" \
-                       +exp_dir="${checkpoint}"
-    done
+    test_dataset="zjumocap_${dataset}_mono_eval_novel"
+    python render.py mode=test \
+                     dataset.test_mode=view \
+                     dataset="${test_dataset}" \
+                     opt.lambda_aiap_xyz="${lambda_xyz}" \
+                     opt.lambda_aiap_cov="${lambda_cov}" \
+                     +exp_dir="${checkpoint}"
   done
 done
